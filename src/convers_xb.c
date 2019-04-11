7b75b6a3e5e2a33ff938729ba9b1e186ad62471a
vagrant <vagrant@This-MacBook-Pro.local>
2019-04-11 07:09:47 -0700

    norm convers_xX

--- src/convers_xb.c
+++ src/convers_xb.c
@@ -63,63 +63,78 @@ char			*xx_zero(t_format *fmt)
 	return (nbr);
 }
 
-static char		*xx_n(t_format *fmt, char *num, int len_num, int base)
+static void		xx_nres_annex(char **res, char *nbr, int len_nbr, int width)
 {
-	int		len_nbr;
-	int		is_dash;
-	int		diff;
-	char		*res;
-	char		*nbr;
-	char		c;
 	int		i;
 	int		j;
 
-	len_num = ft_strlen(num);
-	is_dash = (ft_strchr(fmt->flag, '#')) ? 2 : 0;
-	len_nbr = ft_max(len_num, fmt->precis) + is_dash;
+	i = 0;
+	j = 0;
+	while (j < len_nbr)
+		(*res)[i++] = nbr[j++];
+	while (i < width)
+		(*res)[i++] = ' ';
+}
+
+/*
+**	var[2]		0: i	1: j
+*/
+
+static char		*xx_nres(t_format *fmt, char *nbr, int len_nbr, int base)
+{
+	int		var[2];
+	char		*res;
+	char		c;
+
+	c = (fmt->precis == -1 && ft_strchr(fmt->flag, '0') 
+		&& !ft_strchr(fmt->flag, '-')) ? '0' : ' ';
+	res = (char*)malloc(sizeof(char) * (fmt->width + 1));
+	res[fmt->width] = '\0';
+	if (!ft_strchr(fmt->flag, '-'))
+	{
+		var[0] = -1;
+		while (++(var[0]) < fmt->width - len_nbr)
+			res[var[0]] = c;
+		if (ft_strchr(fmt->flag, '#') && c == '0')
+		{
+			res[1] = (base == 16) ? 'x' : 'b';
+			nbr[1] = '0';
+		}
+		var[1] = -1;
+		while (++(var[1]) < len_nbr)
+			res[(var[0])++] = nbr[var[1]];
+	}
+	else
+		xx_nres_annex(&res, nbr, len_nbr, fmt->width);
+	return (res);
+}
+
+/*
+**	var[3]		0: is_dash	1: i	2: j
+*/
+
+static char		*xx_n(t_format *fmt, char *num, int len_num, int base)
+{
+	int		len_nbr;
+	int		var[3];
+	char		*res;
+	char		*nbr;
+
+	var[0] = (ft_strchr(fmt->flag, '#')) ? 2 : 0;
+	len_nbr = ft_max(len_num, fmt->precis) + var[0];
 	nbr = (char*)malloc(sizeof(char) * (len_nbr + 1));
 	nbr[len_nbr] = '\0';
-	i = len_nbr;
-	j = len_num;
-	while (--j >= 0)
-		nbr[--i] = num[j];
-	while (--i >= 0)
-		nbr[i] = '0';
-	diff = fmt->width - len_nbr;
-	if (is_dash)
-	{
-		nbr[0] = '0';
+	var[1] = len_nbr;
+	var[2] = len_num;
+	while (--(var[2]) >= 0)
+		nbr[--(var[1])] = num[var[2]];
+	while (--(var[1]) >= 0)
+		nbr[var[1]] = '0';
+	if (var[0])
 		nbr[1] = (base == 16) ? 'x' : 'b';
-	}
-	if (diff > 0)
+	if (fmt->width - len_nbr > 0)
 	{
-		c = (fmt->precis == -1 && ft_strchr(fmt->flag, '0') 
-			&& !ft_strchr(fmt->flag, '-')) ? '0' : ' ';
-		res = (char*)malloc(sizeof(char) * (fmt->width + 1));
-		res[fmt->width] = '\0';
-		if (!ft_strchr(fmt->flag, '-'))
-		{
-			i = -1;
-			while (++i < diff)
-				res[i] = c;
-			if (is_dash && c == '0')
-			{
-				res[1] = (base == 16) ? 'x' : 'b';
-				nbr[1] = '0';
-			}
-			j = -1;
-			while (++j < len_nbr)
-				res[i++] = nbr[j];
-		}
-		else
-		{
-			i = 0;
-			j = 0;
-			while (j < len_nbr)
-				res[i++] = nbr[j++];
-			while (i < fmt->width)
-				res[i++] = ' ';
-		}
+		res = xx_nres(fmt, nbr, len_nbr, base);
 		free(nbr);
 		return (res);
 	}
